a54edadc24567692dea2d7ebf93171357abd543f
tk6996 <watcharinkanha@gmail.com>
2021-05-21 21:40:22 +0700

    co-lab navbar

--- Controllers/CoLabController.cs
+++ Controllers/CoLabController.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SlothFlyingWeb.Utils;
 using SlothFlyingWeb.Models;
 
 namespace SlothFlyingWeb.Controllers
@@ -25,38 +27,84 @@ namespace SlothFlyingWeb.Controllers
 
         public async Task<IActionResult> Index()
         {
-            IEnumerable<CoLab> coLabDetails = null;
-
-            HttpClient client = new HttpClient();
-
-            client.BaseAddress = new Uri(_config.GetValue<string>("CoLabApi:ApiUri"));
-
-            HttpResponseMessage response = await client.GetAsync(_config.GetValue<string>("CoLabApi:GetLabPath"));
-
-            if (response.IsSuccessStatusCode)
+            if (HttpContext.Session.GetInt32("Id") == null && HttpContext.Session.GetInt32("AdminId") == null)
             {
-                coLabDetails = await JsonSerializer.DeserializeAsync<IEnumerable<CoLab>>(await response.Content.ReadAsStreamAsync());
+                return RedirectToAction("Login", "User");
             }
 
+            IEnumerable<CoLab> coLabDetails = await _cache.GetOrCreateAsync<IEnumerable<CoLab>>("CoApiLab", async entry =>
+            {
+                HttpClient client = new HttpClient();
+
+                client.BaseAddress = new Uri(_config.GetValue<string>("CoLabApi:ApiUri"));
+
+                HttpResponseMessage response = await client.GetAsync(_config.GetValue<string>("CoLabApi:GetLabPath"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    entry.SlidingExpiration = TimeSpan.FromMinutes(5);
+                    return await Task.FromResult(await JsonSerializer.DeserializeAsync<IEnumerable<CoLab>>(await response.Content.ReadAsStreamAsync()));
+                }
+                else
+                {
+                    entry.SlidingExpiration = null;
+                    return null;
+                }
+            });
             return View(coLabDetails);
         }
 
         public async Task<IActionResult> Lab(int id)
         {
-            HttpClient client = new HttpClient();
-
-            client.BaseAddress = new Uri(_config.GetValue<string>("CoLabApi:ApiUri"));
-
-            HttpResponseMessage response = await client.GetAsync($"{_config.GetValue<string>("CoLabApi:GetLabBookingPath")}/{id}");
-
-            if (response.IsSuccessStatusCode)
+            if (HttpContext.Session.GetInt32("Id") == null && HttpContext.Session.GetInt32("AdminId") == null)
             {
-                CoLabBooking coLabBooking = await JsonSerializer.DeserializeAsync<CoLabBooking>(await response.Content.ReadAsStreamAsync());
-                ViewBag.startDate = DateTime.Parse(coLabBooking.timeStamp).Date;
-                return View(coLabBooking);
+                return RedirectToAction("Login", "User");
             }
 
-            return NotFound();
+            DateTime startDate = BangkokDateTime.now().Date;
+            DateTime cacheDate;
+            CoLabBooking coLabBooking;
+
+            Func<DateTime, object, bool> checkDateCurrent = (cacheDate, key) =>
+             {
+                 if (cacheDate < startDate)
+                 {
+                     _cache.Remove(key);
+                     return true;
+                 }
+                 return false;
+             };
+
+            do
+            {
+                (coLabBooking, cacheDate) = await _cache.GetOrCreateAsync<(CoLabBooking, DateTime)>($"CoApiBooking_{id}", async entry =>
+                 {
+                     HttpClient client = new HttpClient();
+
+                     client.BaseAddress = new Uri(_config.GetValue<string>("CoLabApi:ApiUri"));
+
+                     HttpResponseMessage response = await client.GetAsync($"{_config.GetValue<string>("CoLabApi:GetLabBookingPath")}/{id}");
+
+                     if (response.IsSuccessStatusCode)
+                     {
+                         entry.SlidingExpiration = TimeSpan.FromMinutes(1);
+                         return await Task.FromResult((await JsonSerializer.DeserializeAsync<CoLabBooking>(await response.Content.ReadAsStreamAsync()), startDate));
+                     }
+                     else
+                     {
+                         entry.SlidingExpiration = null;
+                         return (null, startDate);
+                     }
+                 });
+            } while (checkDateCurrent(cacheDate, $"CoApiBooking_{id}"));
+
+            if(coLabBooking == null)
+            {
+                return NotFound();
+            }
+            
+            ViewBag.startDate = DateTime.Parse(coLabBooking.timeStamp).Date;
+            return View(coLabBooking);
         }
     }
 }
